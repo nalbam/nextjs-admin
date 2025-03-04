bfb86a72d84e8cf737bb3843372d827719775271
nalbam <me@nalbam.com>
2025-03-04 22:04:16 +0900

    Update navigation links in dashboard layout

---
+++ app/(dashboard)/orders/page.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+
+export default function OrdersPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>View all orders.</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+
+export default function HomePage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Dashboard</CardTitle>
+        <CardDescription>Dashboard Home.</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
+}
